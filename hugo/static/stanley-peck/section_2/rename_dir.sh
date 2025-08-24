43bf07fcbfcd98785a56a3185c2836ccfab7e017
Tim Gibbon <tng@chegwin.org>
2025-08-24 17:48:59 +0100

    Clear cache

--- hugo/static/stanley-peck/section_2/rename_dir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in *
-do 
-mv "$file" "$(echo "$file" | tr ' ' '_')" 
-done
-
--- hugo/static/stanley-peck/section_3_stanleys_school_letters/rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for FILE in *
-do
-SFILE=$(echo $FILE | sed 's/_(DocTitle)_/_/g') 
-echo mv \'$FILE\' $SFILE
-mv $FILE $SFILE
-###mv $FILE $SFILE
-done
